19e97d4d2c430cf7e8a6e91fa1116c239229c61c
Nakul Kumar <nak.ak2015@gmail.com>
2023-10-26 11:52:46 -0400

    Added some comments

--- testgan.py
+++ testgan.py
@@ -1,6 +1,7 @@
 import dataloader
 import tensorflow as tf
 
+## testing loading data and moving to GPU VRAM
 test = dataloader.MusicLoader(
     10, "./fma_small_genres.csv", "./data/fma_small/")
 
