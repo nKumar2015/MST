85e0fa44279e64ec2d857a0b92f65b7ca03f6e9d
Nakul Kumar <70525511+nKumar2015@users.noreply.github.com>
2023-12-01 15:02:01 -0500

    Fished datset fma_dataset_builder. Working on integrating with the gan.

--- fma/fma_dataset_builder.py
+++ fma/fma_dataset_builder.py
@@ -2,70 +2,110 @@
 
 import tensorflow_datasets as tfds
 import numpy as np
+import librosa
+import tensorflow as tf
+import matplotlib.pyplot as plt
 
 class Builder(tfds.core.GeneratorBasedBuilder):
-  """DatasetBuilder for fma dataset."""
+    """DatasetBuilder for fma dataset."""
 
-  skiplist = ["011298.mp3",
-            "021657.mp3",
-            "029245.mp3",
-            "054568.mp3",
-            "054576.mp3",
-            "098565.mp3",
-            "098567.mp3",
-            "098569.mp3",
-            "099234.mp3",
-            "099134.mp3",
-            "108925.mp3",
-            "133297.mp3",
-            "155066.mp3"]
+    skiplist = ["011298.mp3",
+                "021657.mp3",
+                "029245.mp3",
+                "054568.mp3",
+                "054576.mp3",
+                "098565.mp3",
+                "098567.mp3",
+                "098569.mp3",
+                "099234.mp3",
+                "099134.mp3",
+                "108925.mp3",
+                "133297.mp3",
+                "155066.mp3"]
 
-  VERSION = tfds.core.Version('1.0.0')
-  RELEASE_NOTES = {
-      '1.0.0': 'Initial release.',
-  }
-
-  def _info(self) -> tfds.core.DatasetInfo:
-    """Returns the dataset metadata."""
-    # TODO(fma): Specifies the tfds.core.DatasetInfo object
-    return self.dataset_info_from_configs(
-        features=tfds.features.FeaturesDict({
-            # These are the features of your dataset like images, labels ...
-            'audio_id': tfds.features.Text(),
-            'genre': tfds.features.ClassLabel(names=['Hip-Hop', 'Pop', 'Folk', 'Avant-Garde', 'Lo-Fi', 'Rock', 'Metal', 'Post-Punk', 'Krautrock', 'Punk', 'Electroacoustic', 'Reggae - Dub', 'Latin America', 'International', 'Free-Folk', 'Noise', 'Noise-Rock', 'Audio Collage', 'Drone', 'Electronic', 'Psych-Folk', 'Field Recordings', 'Psych-Rock', 'Experimental', 'Progressive', 'Experimental Pop', 'Balkan', 'Hardcore', 'Singer-Songwriter', 'Polka', 'African', 'French', 'Middle East', 'Sound Collage', 'Freak-Folk', 'Death-Metal', 'Garage', 'Electro-Punk', 'Alternative Hip-Hop', 'Compilation', 'Unclassifiable', 'Industrial', 'IDM', 'New Wave', 'Drum & Bass', 'Holiday', 'Trip-Hop', 'North African', 'Chip Music', 'Breakcore - Hard', 'Soundtrack', 'Indie-Rock', 'Synth Pop', 'Indian', 'Asia-Far East', 'Afrobeat', 'Black-Metal', 'Chiptune', 'Rock Opera', 'Space-Rock', 'Brazilian', 'Techno', 'Ambient Electronic', 'Dubstep', 'Europe', 'Tango', 'House', 'Improv', 'Sound Poetry', 'Minimal Electronic', 'Breakbeat', 'Novelty', 'No Wave', 'Ambient', 'British Folk', 'Downtempo', 'Power-Pop', 'Minimalism', 'Surf', 'Celtic', 'Sound Art', 'Post-Rock', 'Klezmer', 'Romany (Gypsy)', 'Kid-Friendly', 'Salsa', 'Chill-out', 'Shoegaze', 'Hip-Hop Beats', 'Rap', 'Dance', 'Goth', 'Grindcore', 'Instrumental', 'New Age', 'Glitch', 'Latin', 'Jungle', 'Reggae - Dancehall', 'Turkish', 'Cumbia', 'Nerdcore', 'Loud-Rock']),
-        }),
-        # If there's a common (input, target) tuple from the
-        # features, specify them here. They'll be used if
-        # `as_supervised=True` in `builder.as_dataset`.
-        supervised_keys=('audio', 'genre'),  # Set to `None` to disable
-        homepage='https://github.com/mdeff/fma',
-    )
-
-  def _split_generators(self, dl_manager: tfds.download.DownloadManager):
-    """Returns SplitGenerators."""
-    # TODO(fma): Downloads the data and defines the splits
-    path = dl_manager.download_and_extract('https://zenodo.org/records/10223581/files/fma.zip?download=1')
-    
-
-    # TODO(fma): Returns the Dict[split names, Iterator[Key, Example]]
-    return {
-        'train_style': self._generate_examples(path / 'fma/train_style', path/"fma/genres.csv"),
-        'train_content': self._generate_examples(path / 'fma/train_content', path/"fma/genres.csv"),
-        'test_style': self._generate_examples(path / 'fma/test_style', path/"fma/genres.csv"),
-        'test_content': self._generate_examples(path / 'fma/test_content', path/"fma/genres.csv"),
+    VERSION = tfds.core.Version('1.0.0')
+    RELEASE_NOTES = {
+        '1.0.0': 'Initial Release',
     }
 
-  def _generate_examples(self, path, genres):
-    """Yields examples."""
-    # TODO(fma): Yields (key, example) tuples from the dataset
-    data = np.loadtxt(genres, delimiter=",", dtype=str, skiprows=1)
-    for f in path.glob('*.mp3'):
-      pathstring = str(f)
-      id = pathstring[-10:]
-      genre = data[data[:, 0] == id][0,1][3:-1]
-      key = id[:-3]
-      if f not in self.skiplist:
-        yield (key, {
-            'audio_id': pathstring,
-            'genre': data[data[:, 0] == id][0,1][3:-1],
-        })
+    def scale_minmax(self, X, min=0.0, max=1.0):
+        X_std = (X - X.min()) / (X.max() - X.min())
+        X_scaled = X_std * (max - min) + min
+        return X_scaled
+
+    def load_spectrogram(self, path):
+        samples, sr = librosa.load(path, sr=None)
+        sgram = librosa.stft(samples)
+
+        sgram_mag, _ = librosa.magphase(sgram)
+
+        mels = librosa.feature.melspectrogram(S=sgram_mag, sr=sr)
+        mels = librosa.amplitude_to_db(mels, ref=np.min)
+        mels = np.log(mels + 1e-9)
+        mels = self.scale_minmax(mels, 0, 255).astype(np.uint8)
+        mels = np.dstack((mels, mels, mels))
+        return mels, sr
+
+    def _info(self) -> tfds.core.DatasetInfo:
+        """Returns the dataset metadata."""
+        # TODO(fma): Specifies the tfds.core.DatasetInfo object
+        return self.dataset_info_from_configs(
+            features=tfds.features.FeaturesDict({
+                # These are the features of your dataset like images, labels ...
+                'sgram_image': tfds.features.Image(),
+                'sample_rate': tfds.features.Scalar(dtype=tf.float32),
+                'genre': tfds.features.ClassLabel(names=['Hip-Hop', 'Pop', 'Folk', 'Avant-Garde', 'Lo-Fi', 'Rock', 'Metal',
+                                                         'Post-Punk', 'Krautrock', 'Punk', 'Electroacoustic', 'Reggae - Dub',
+                                                         'Latin America', 'International', 'Free-Folk', 'Noise', 'Noise-Rock',
+                                                         'Audio Collage', 'Drone', 'Electronic', 'Psych-Folk', 'Field Recordings',
+                                                         'Psych-Rock', 'Experimental', 'Progressive', 'Experimental Pop', 'Balkan',
+                                                         'Hardcore', 'Singer-Songwriter', 'Polka', 'African', 'French', 'Middle East',
+                                                         'Sound Collage', 'Freak-Folk', 'Death-Metal', 'Garage', 'Electro-Punk',
+                                                         'Alternative Hip-Hop', 'Compilation', 'Unclassifiable', 'Industrial', 'IDM',
+                                                         'New Wave', 'Drum & Bass', 'Holiday', 'Trip-Hop', 'North African', 'Chip Music',
+                                                         'Breakcore - Hard', 'Soundtrack', 'Indie-Rock', 'Synth Pop', 'Indian', 'Asia-Far East',
+                                                         'Afrobeat', 'Black-Metal', 'Chiptune', 'Rock Opera', 'Space-Rock', 'Brazilian', 'Techno',
+                                                         'Ambient Electronic', 'Dubstep', 'Europe', 'Tango', 'House', 'Improv', 'Sound Poetry',
+                                                         'Minimal Electronic', 'Breakbeat', 'Novelty', 'No Wave', 'Ambient', 'British Folk', 'Downtempo',
+                                                         'Power-Pop', 'Minimalism', 'Surf', 'Celtic', 'Sound Art', 'Post-Rock', 'Klezmer', 'Romany (Gypsy)',
+                                                         'Kid-Friendly', 'Salsa', 'Chill-out', 'Shoegaze', 'Hip-Hop Beats', 'Rap', 'Dance', 'Goth', 'Grindcore',
+                                                         'Instrumental', 'New Age', 'Glitch', 'Latin', 'Jungle', 'Reggae - Dancehall', 'Turkish', 'Cumbia', 'Nerdcore', 'Loud-Rock']),
+            }),
+            # If there's a common (input, target) tuple from the
+            # features, specify them here. They'll be used if
+            # `as_supervised=True` in `builder.as_dataset`.
+            # Set to `None` to disable
+            supervised_keys=('sgram', 'sample_rate', 'genre'),
+            homepage='https://github.com/mdeff/fma',
+        )
+
+    def _split_generators(self, dl_manager: tfds.download.DownloadManager):
+        """Returns SplitGenerators."""
+        # TODO(fma): Downloads the data and defines the splits
+        path = dl_manager.download_and_extract(
+            'https://zenodo.org/records/10223581/files/fma.zip?download=1')
+
+        # TODO(fma): Returns the Dict[split names, Iterator[Key, Example]]
+        return {
+            'train_style': self._generate_examples(path / 'fma/train_style', path/"fma/genres.csv"),
+            'train_content': self._generate_examples(path / 'fma/train_content', path/"fma/genres.csv"),
+            'test_style': self._generate_examples(path / 'fma/test_style', path/"fma/genres.csv"),
+            'test_content': self._generate_examples(path / 'fma/test_content', path/"fma/genres.csv"),
+        }
+
+    def _generate_examples(self, path, genres):
+        """Yields examples."""
+        # TODO(fma): Yields (key, example) tuples from the dataset
+        data = np.loadtxt(genres, delimiter=",", dtype=str, skiprows=1)
+        for f in path.glob('*.mp3'):
+            pathstring = str(f)
+            id = pathstring[-10:]
+            genre = data[data[:, 0] == id][0, 1][3:-1]
+            key = id[:-3]
+            sgram, sample_rate = self.load_spectrogram(pathstring)
+            if f not in self.skiplist:
+                yield (key, {
+                    'sgram_image': sgram,
+                    'sample_rate': sample_rate,
+                    'genre': genre,
+                })
--- testgan.py
+++ testgan.py
@@ -3,157 +3,125 @@ import tensorflow as tf
 import tensorflow_datasets as tfds
 from tensorflow_examples.models.pix2pix import pix2pix
 
-import os 
+import os
 import time
 import matplotlib.pyplot as plt
 from IPython.display import clear_output
+import librosa
+import numpy as np
 
 AUTOTUNE = tf.data.AUTOTUNE
 
 # testing loading data and moving to GPU VRAM
-#test = dataloader.MusicLoader(
+# test = dataloader.MusicLoader(
 #    10, "./fma_small_genres.csv", "./data/fma_small/")
-#x = test.getX()
+# x = test.getX()
 
-dataset, metadata = tfds.load('cycle_gan/horse2zebra', with_info=True, as_supervised=True)
-train_horses, train_zebras = dataset['trainA'], dataset['trainB']
-test_horses, test_zebras = dataset['testA'], dataset['testB']
+dataset = tfds.load('fma', with_info=True, as_supervised=False)[0]
+train_style, train_content = dataset['train_style'], dataset['train_content']
+test_style, test_content = dataset['test_style'], dataset['test_content']
 
 BUFFER_SIZE = 1000
 BATCH_SIZE = 1
 IMG_WIDTH = 256
 IMG_HEIGHT = 256
 
-def random_crop(image):
-  cropped_image = tf.image.random_crop(
-      image, size=[IMG_HEIGHT, IMG_WIDTH, 3])
 
-  return cropped_image
+def load_spectrogram(path):
+    samples, sample_rate = librosa.load(path, sr=None)
+    sgram = librosa.stft(samples)
+    sgram_mag, _ = librosa.magphase(sgram)
+    mel_scale_sgram = librosa.feature.melspectrogram(
+        S=sgram_mag, sr=sample_rate)
+    mel_sgram = librosa.amplitude_to_db(mel_scale_sgram, ref=np.min)
+    return mel_sgram
 
 # normalizing the images to [-1, 1]
+
+
 def normalize(image):
-  image = tf.cast(image, tf.float32)
-  image = (image / 127.5) - 1
-  return image
+    image = tf.cast(image, tf.float32)
+    image = (image / 127.5) - 1
+    return image
 
-def random_jitter(image):
-  # resizing to 286 x 286 x 3
-  image = tf.image.resize(image, [286, 286],
-                          method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
 
-  # randomly cropping to 256 x 256 x 3
-  image = random_crop(image)
+def preprocess_audio_train(item):
+    # mel_sgram = load_spectrogram(item)
+    # image = normalize(mel_sgram)
+    return item
 
-  # random mirroring
-  image = tf.image.random_flip_left_right(image)
+def preprocess_audio_test(item):
+    # mel_sgram = load_spectrogram(item['audio_id'])
+    # image = normalize(mel_sgram)
+    return item
 
-  return image
 
-def preprocess_image_train(image, label):
-  image = random_jitter(image)
-  image = normalize(image)
-  return image
-
-def preprocess_image_test(image, label):
-  image = normalize(image)
-  return image
-
-train_horses = train_horses.cache().map(
-    preprocess_image_train, num_parallel_calls=AUTOTUNE).shuffle(
+train_content = train_content.cache().map(
+    preprocess_audio_train, num_parallel_calls=AUTOTUNE).shuffle(
     BUFFER_SIZE).batch(BATCH_SIZE)
 
-train_zebras = train_zebras.cache().map(
-    preprocess_image_train, num_parallel_calls=AUTOTUNE).shuffle(
+train_style = train_style.cache().map(
+    preprocess_audio_train, num_parallel_calls=AUTOTUNE).shuffle(
     BUFFER_SIZE).batch(BATCH_SIZE)
 
-test_horses = test_horses.map(
-    preprocess_image_test, num_parallel_calls=AUTOTUNE).cache().shuffle(
+test_content = test_content.map(
+    preprocess_audio_test, num_parallel_calls=AUTOTUNE).cache().shuffle(
     BUFFER_SIZE).batch(BATCH_SIZE)
 
-test_zebras = test_zebras.map(
-    preprocess_image_test, num_parallel_calls=AUTOTUNE).cache().shuffle(
+test_style = test_style.map(
+    preprocess_audio_test, num_parallel_calls=AUTOTUNE).cache().shuffle(
     BUFFER_SIZE).batch(BATCH_SIZE)
 
-sample_horse = next(iter(train_horses))
-sample_zebra = next(iter(train_zebras))
+sample_content = next(iter(train_content))
+sample_style = next(iter(train_style))
 
-plt.subplot(121)
-plt.title('Horse')
-plt.imshow(sample_horse[0] * 0.5 + 0.5)
+plt.title('Content Song')
+librosa.display.specshow(
+    sample_content['sgram_image'][0].numpy(), sr=float(sample_content['sample_rate']), x_axis='time', y_axis='mel')
+plt.colorbar(format='%+2.0f dB')
+plt.show()
 
-plt.subplot(122)
-plt.title('Horse with random jitter')
-plt.imshow(random_jitter(sample_horse[0]) * 0.5 + 0.5)
-
-plt.subplot(121)
-plt.title('Zebra')
-plt.imshow(sample_zebra[0] * 0.5 + 0.5)
-
-plt.subplot(122)
-plt.title('Zebra with random jitter')
-plt.imshow(random_jitter(sample_zebra[0]) * 0.5 + 0.5)
+plt.title('Style Song')
+librosa.display.specshow(
+    sample_style['sgram_image'][0].numpy(), sr=float(sample_style['sample_rate']), x_axis='time', y_axis='mel')
+plt.colorbar(format='%+2.0f dB')
+plt.show()
 
 OUTPUT_CHANNELS = 3
 
-generator_g = pix2pix.unet_generator(OUTPUT_CHANNELS, norm_type='instancenorm')
-generator_f = pix2pix.unet_generator(OUTPUT_CHANNELS, norm_type='instancenorm')
+generator_g = pix2pix.unet_generator(1, norm_type='instancenorm')
+generator_f = pix2pix.unet_generator(1, norm_type='instancenorm')
 
 discriminator_x = pix2pix.discriminator(norm_type='instancenorm', target=False)
 discriminator_y = pix2pix.discriminator(norm_type='instancenorm', target=False)
 
-to_zebra = generator_g(sample_horse)
-to_horse = generator_f(sample_zebra)
-plt.figure(figsize=(8, 8))
+
 contrast = 8
-
-imgs = [sample_horse, to_zebra, sample_zebra, to_horse]
-title = ['Horse', 'To Zebra', 'Zebra', 'To Horse']
-
-for i in range(len(imgs)):
-  plt.subplot(2, 2, i+1)
-  plt.title(title[i])
-  if i % 2 == 0:
-    plt.imshow(imgs[i][0] * 0.5 + 0.5)
-  else:
-    plt.imshow(imgs[i][0] * 0.5 * contrast + 0.5)
-plt.show()
-
-plt.figure(figsize=(8, 8))
-
-plt.subplot(121)
-plt.title('Is a real zebra?')
-plt.imshow(discriminator_y(sample_zebra)[0, ..., -1], cmap='RdBu_r')
-
-plt.subplot(122)
-plt.title('Is a real horse?')
-plt.imshow(discriminator_x(sample_horse)[0, ..., -1], cmap='RdBu_r')
-
-plt.show()
 
 LAMBDA = 10
 
 loss_obj = tf.keras.losses.BinaryCrossentropy(from_logits=True)
 
 def discriminator_loss(real, generated):
-  real_loss = loss_obj(tf.ones_like(real), real)
+    real_loss = loss_obj(tf.ones_like(real), real)
 
-  generated_loss = loss_obj(tf.zeros_like(generated), generated)
+    generated_loss = loss_obj(tf.zeros_like(generated), generated)
 
-  total_disc_loss = real_loss + generated_loss
+    total_disc_loss = real_loss + generated_loss
 
-  return total_disc_loss * 0.5
+    return total_disc_loss * 0.5
 
 def generator_loss(generated):
-  return loss_obj(tf.ones_like(generated), generated)
+    return loss_obj(tf.ones_like(generated), generated)
 
 def calc_cycle_loss(real_image, cycled_image):
-  loss1 = tf.reduce_mean(tf.abs(real_image - cycled_image))
+    loss1 = tf.reduce_mean(tf.abs(real_image - cycled_image))
 
-  return LAMBDA * loss1
+    return LAMBDA * loss1
 
 def identity_loss(real_image, same_image):
-  loss = tf.reduce_mean(tf.abs(real_image - same_image))
-  return LAMBDA * 0.5 * loss
+    loss = tf.reduce_mean(tf.abs(real_image - same_image))
+    return LAMBDA * 0.5 * loss
 
 generator_g_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
 generator_f_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
@@ -176,111 +144,114 @@ ckpt_manager = tf.train.CheckpointManager(ckpt, checkpoint_path, max_to_keep=5)
 
 # if a checkpoint exists, restore the latest checkpoint.
 if ckpt_manager.latest_checkpoint:
-  ckpt.restore(ckpt_manager.latest_checkpoint)
-  print ('Latest checkpoint restored!!')
+    ckpt.restore(ckpt_manager.latest_checkpoint)
+    print('Latest checkpoint restored!!')
 
 EPOCHS = 10
 
 def generate_images(model, test_input):
-  prediction = model(test_input)
+    prediction = model(test_input)
 
-  plt.figure(figsize=(12, 12))
+    plt.figure(figsize=(12, 12))
 
-  display_list = [test_input[0], prediction[0]]
-  title = ['Input Image', 'Predicted Image']
+    display_list = [test_input[0], prediction[0]]
+    title = ['Input Image', 'Predicted Image']
 
-  for i in range(2):
-    plt.subplot(1, 2, i+1)
-    plt.title(title[i])
-    # getting the pixel values between [0, 1] to plot it.
-    plt.imshow(display_list[i] * 0.5 + 0.5)
-    plt.axis('off')
-  plt.show()
+    for i in range(2):
+        plt.subplot(1, 2, i+1)
+        plt.title(title[i])
+        # getting the pixel values between [0, 1] to plot it.
+        plt.imshow(display_list[i] * 0.5 + 0.5)
+        plt.axis('off')
+    plt.show()
 
 @tf.function
 def train_step(real_x, real_y):
-  # persistent is set to True because the tape is used more than
-  # once to calculate the gradients.
-  with tf.GradientTape(persistent=True) as tape:
-    # Generator G translates X -> Y
-    # Generator F translates Y -> X.
+    # persistent is set to True because the tape is used more than
+    # once to calculate the gradients.
+    with tf.GradientTape(persistent=True) as tape:
+        # Generator G translates X -> Y
+        # Generator F translates Y -> X.
 
-    fake_y = generator_g(real_x, training=True)
-    cycled_x = generator_f(fake_y, training=True)
+        fake_y = generator_g(real_x, training=True)
+        cycled_x = generator_f(fake_y, training=True)
 
-    fake_x = generator_f(real_y, training=True)
-    cycled_y = generator_g(fake_x, training=True)
+        fake_x = generator_f(real_y, training=True)
+        cycled_y = generator_g(fake_x, training=True)
 
-    # same_x and same_y are used for identity loss.
-    same_x = generator_f(real_x, training=True)
-    same_y = generator_g(real_y, training=True)
+        # same_x and same_y are used for identity loss.
+        same_x = generator_f(real_x, training=True)
+        same_y = generator_g(real_y, training=True)
 
-    disc_real_x = discriminator_x(real_x, training=True)
-    disc_real_y = discriminator_y(real_y, training=True)
+        disc_real_x = discriminator_x(real_x, training=True)
+        disc_real_y = discriminator_y(real_y, training=True)
 
-    disc_fake_x = discriminator_x(fake_x, training=True)
-    disc_fake_y = discriminator_y(fake_y, training=True)
+        disc_fake_x = discriminator_x(fake_x, training=True)
+        disc_fake_y = discriminator_y(fake_y, training=True)
 
-    # calculate the loss
-    gen_g_loss = generator_loss(disc_fake_y)
-    gen_f_loss = generator_loss(disc_fake_x)
+        # calculate the loss
+        gen_g_loss = generator_loss(disc_fake_y)
+        gen_f_loss = generator_loss(disc_fake_x)
 
-    total_cycle_loss = calc_cycle_loss(real_x, cycled_x) + calc_cycle_loss(real_y, cycled_y)
+        total_cycle_loss = calc_cycle_loss(
+            real_x, cycled_x) + calc_cycle_loss(real_y, cycled_y)
 
-    # Total generator loss = adversarial loss + cycle loss
-    total_gen_g_loss = gen_g_loss + total_cycle_loss + identity_loss(real_y, same_y)
-    total_gen_f_loss = gen_f_loss + total_cycle_loss + identity_loss(real_x, same_x)
+        # Total generator loss = adversarial loss + cycle loss
+        total_gen_g_loss = gen_g_loss + \
+            total_cycle_loss + identity_loss(real_y, same_y)
+        total_gen_f_loss = gen_f_loss + \
+            total_cycle_loss + identity_loss(real_x, same_x)
 
-    disc_x_loss = discriminator_loss(disc_real_x, disc_fake_x)
-    disc_y_loss = discriminator_loss(disc_real_y, disc_fake_y)
+        disc_x_loss = discriminator_loss(disc_real_x, disc_fake_x)
+        disc_y_loss = discriminator_loss(disc_real_y, disc_fake_y)
 
-  # Calculate the gradients for generator and discriminator
-  generator_g_gradients = tape.gradient(total_gen_g_loss, 
-                                        generator_g.trainable_variables)
-  generator_f_gradients = tape.gradient(total_gen_f_loss, 
-                                        generator_f.trainable_variables)
+    # Calculate the gradients for generator and discriminator
+    generator_g_gradients = tape.gradient(total_gen_g_loss,
+                                          generator_g.trainable_variables)
+    generator_f_gradients = tape.gradient(total_gen_f_loss,
+                                          generator_f.trainable_variables)
 
-  discriminator_x_gradients = tape.gradient(disc_x_loss, 
-                                            discriminator_x.trainable_variables)
-  discriminator_y_gradients = tape.gradient(disc_y_loss, 
-                                            discriminator_y.trainable_variables)
+    discriminator_x_gradients = tape.gradient(disc_x_loss,
+                                              discriminator_x.trainable_variables)
+    discriminator_y_gradients = tape.gradient(disc_y_loss,
+                                              discriminator_y.trainable_variables)
 
-  # Apply the gradients to the optimizer
-  generator_g_optimizer.apply_gradients(zip(generator_g_gradients, 
-                                            generator_g.trainable_variables))
+    # Apply the gradients to the optimizer
+    generator_g_optimizer.apply_gradients(zip(generator_g_gradients,
+                                              generator_g.trainable_variables))
 
-  generator_f_optimizer.apply_gradients(zip(generator_f_gradients, 
-                                            generator_f.trainable_variables))
+    generator_f_optimizer.apply_gradients(zip(generator_f_gradients,
+                                              generator_f.trainable_variables))
 
-  discriminator_x_optimizer.apply_gradients(zip(discriminator_x_gradients,
-                                                discriminator_x.trainable_variables))
+    discriminator_x_optimizer.apply_gradients(zip(discriminator_x_gradients,
+                                                  discriminator_x.trainable_variables))
 
-  discriminator_y_optimizer.apply_gradients(zip(discriminator_y_gradients,
-                                                discriminator_y.trainable_variables))
+    discriminator_y_optimizer.apply_gradients(zip(discriminator_y_gradients,
+                                                  discriminator_y.trainable_variables))
+
 for epoch in range(EPOCHS):
-  start = time.time()
+    start = time.time()
 
-  n = 0
-  for image_x, image_y in tf.data.Dataset.zip((train_horses, train_zebras)):
-    train_step(image_x, image_y)
-    if n % 10 == 0:
-      print ('.', end='')
-    n += 1
+    n = 0
+    for image_x, image_y in tf.data.Dataset.zip((train_content, train_style)):
+        train_step(image_x, image_y)
+        if n % 10 == 0:
+            print('.', end='')
+        n += 1
 
-  clear_output(wait=True)
-  # Using a consistent image (sample_horse) so that the progress of the model
-  # is clearly visible.
-  generate_images(generator_g, sample_horse)
+    clear_output(wait=True)
+    # Using a consistent image (sample_horse) so that the progress of the model
+    # is clearly visible.
+    generate_images(generator_g, sample_style)
 
-  if (epoch + 1) % 5 == 0:
-    ckpt_save_path = ckpt_manager.save()
-    print ('Saving checkpoint for epoch {} at {}'.format(epoch+1,
-                                                         ckpt_save_path))
+    if (epoch + 1) % 5 == 0:
+        ckpt_save_path = ckpt_manager.save()
+        print('Saving checkpoint for epoch {} at {}'.format(epoch+1,
+                                                            ckpt_save_path))
 
-  print ('Time taken for epoch {} is {} sec\n'.format(epoch + 1,
-                                                      time.time()-start))
-  
+    print('Time taken for epoch {} is {} sec\n'.format(epoch + 1,
+                                                       time.time()-start))
+
 # Run the trained model on the test dataset
-for inp in test_horses.take(5):
-  generate_images(generator_g, inp)
-
+for inp in test_content.take(5):
+    generate_images(generator_g, inp)
