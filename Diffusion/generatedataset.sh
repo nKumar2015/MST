8a856d9c5a9e21ec0a3698fa6150ca1a186ce696
Nakul Kumar <70525511+nKumar2015@users.noreply.github.com>
2024-01-26 09:55:46 -0500

    clean

--- Diffusion/generatedataset.sh
+++
@@ -1,5 +0,0 @@
-python scripts/audio_to_images.py \
---resolution 256 \
---input_dir data/fma \
---output_dir data/audio-diffusion-256 \
---push_to_hub teticio/audio-diffusion-256
--- Diffusion/setup.sh
+++
@@ -1,6 +0,0 @@
-git clone https://github.com/huggingface/diffusers.git
-cd diffusers
-
-pip install -e ".[torch]"
-
-pip install -e ".[training]"
